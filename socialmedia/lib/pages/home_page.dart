03dc82dc1cc0808200456a1f2ff73c26e5485e93
milkbreadzee <riaelisabeths@gmail.com>
2023-05-10 23:17:42 +0530

    home page ui

--- socialmedia/lib/pages/home_page.dart
+++ socialmedia/lib/pages/home_page.dart
@@ -98,16 +98,148 @@ class _HomePageState extends State<HomePage> {
               controller: _controller,
               count: 4,
               effect: ExpandingDotsEffect(
-                activeDotColor: Colors.pink.shade400,
-                radius: 25.0,
-                dotWidth: 8.0,
-                dotHeight: 8.0,
-                dotColor: Colors.pink.shade100
-              ),
-            )
+                  activeDotColor: Colors.pink.shade400,
+                  radius: 25.0,
+                  dotWidth: 8.0,
+                  dotHeight: 8.0,
+                  dotColor: Colors.pink.shade100),
+            ),
             //3 buttons send, pay, bills
-            
+            const SizedBox(
+              height: 25,
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  //send
+                  Column(
+                    children: [
+                      Container(
+                        height: 80,
+                        padding: EdgeInsets.all(18),
+                        decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Center(
+                          child: Image.asset('lib/icons/transaction.png'),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'Send',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.grey[600]),
+                      ),
+                    ],
+                  ),
+
+                  const SizedBox(
+                    width: 25,
+                  ),
+
+                  //pay
+                  Column(
+                    children: [
+                      Container(
+                        height: 80,
+                        padding: EdgeInsets.all(18),
+                        decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Center(
+                          child: Image.asset('lib/icons/pay-per-click.png'),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'Pay',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.grey[600]),
+                      ),
+                    ],
+                  ),
+
+                  const SizedBox(
+                    width: 25,
+                  ),
+
+                  //bills
+                  Column(
+                    children: [
+                      Container(
+                        height: 80,
+                        padding: EdgeInsets.all(18),
+                        decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Center(
+                          child: Image.asset('lib/icons/bill.png'),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'Bills',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.grey[600]),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
             //column
+
+            const SizedBox(
+              height: 25,
+            ),
+
+            ListView(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: ListTile(
+                    leading: SizedBox(
+                        height: 40,
+                        width: 40,
+                        child: Image.asset('lib/icons/analysis.png')),
+                    title: const Text('Statistics'),
+                    subtitle: const Text('Payments and Income'),
+                    trailing: Icon(Icons.arrow_forward_ios),
+                    dense: true,
+                  ),
+                ),
+                const SizedBox(height: 10,),
+                 Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: ListTile(
+                    leading: SizedBox(
+                        height: 40,
+                        width: 40,
+                        child: Image.asset('lib/icons/analysis.png')),
+                    title: const Text('Statistics'),
+                    subtitle: const Text('Payments and Income'),
+                    trailing: Icon(Icons.arrow_forward_ios),
+                    dense: true,
+                  ),
+                ),
+                 
+              ],
+            )
           ],
         ),
       ),
